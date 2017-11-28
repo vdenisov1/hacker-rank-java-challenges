5c1759628df8654f0102333b0cbc4356f8e0f5d3
Vlad Denisov <vlad@vdenisov.net>
2017-11-28 13:46:29 -0500

    Message Digest lesson complete

---
+++ src/HashGenerator.java
@@ -0,0 +1,16 @@
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class HashGenerator {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner in = new Scanner(System.in);
+        String pw = in.nextLine();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(pw.getBytes());
+        String hash = DatatypeConverter.printHexBinary(md.digest()).toLowerCase();
+        System.out.println(hash);
+    }
+}
