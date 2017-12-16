c90beb5f67d156fb7d179a63f7ea06857194f9cc
Vlad Denisov <vlad@vdensiov.net>
2017-12-15 20:14:04 -0500

    Finished BitSet tutorial

---
+++ src/edu/hackerrank/datastructures/MyBitSet.java
@@ -0,0 +1,37 @@
+package edu.hackerrank.datastructures;
+
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class MyBitSet {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+        BitSet[] bitSetSet = { new BitSet(N), new BitSet(N) };
+
+        for(int i = 0; i < M; i++){
+            String command = in.next();
+            int input1 = in.nextInt();
+            int input2 = in.nextInt();
+
+            if(command.equals("AND"))
+                bitSetSet[input1 - 1].and(bitSetSet[input2 - 1]);
+
+            if(command.equals("OR"))
+                bitSetSet[input1 - 1].or(bitSetSet[input2 - 1]);
+
+            if(command.equals("XOR"))
+                bitSetSet[input1 - 1].xor(bitSetSet[input2 - 1]);
+
+            if(command.equals("FLIP"))
+                bitSetSet[input1 - 1].flip(input2);
+
+            if(command.equals("SET"))
+                bitSetSet[input1 - 1].set(input2);
+
+            System.out.format("%d %d\n", bitSetSet[0].cardinality(), bitSetSet[1].cardinality());
+        }
+    }
+}
--- test/edu/hackerrank/datastructures/MyHashsetTest.java
+++
@@ -1,8 +0,0 @@
-package edu.hackerrank.datastructures;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class MyHashsetTest {
-
-}
