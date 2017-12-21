2641f29ec883e7e311c1e62a209962f2c5220c92
Vlad Denisov <vlad@vdenisov.net>
2017-12-20 21:29:42 -0500

    Finished Visitor Pattern example

--- src/edu/hackerrank/advanced/VisitorPatternExample.java
+++ src/edu/hackerrank/advanced/VisitorPatternExample.java
@@ -1,9 +1,10 @@
 package edu.hackerrank.advanced;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import javax.swing.text.StyledEditorKit;
+import java.lang.reflect.Array;
+import java.util.*;
 
 enum Color {
   RED, GREEN
@@ -77,9 +78,10 @@ abstract class TreeVis
 }
 
 class SumInLeavesVisitor extends TreeVis {
+  private int result = 0;
+
   public int getResult() {
-    //implement this
-    return 0;
+    return result;
   }
 
   public void visitNode(TreeNode node) {
@@ -87,65 +89,126 @@ class SumInLeavesVisitor extends TreeVis {
   }
 
   public void visitLeaf(TreeLeaf leaf) {
-    //implement this
+    result += leaf.getValue();
   }
 }
 
 class ProductOfRedNodesVisitor extends TreeVis {
+  private long product = 1;
+  private long modulo = (int) Math.pow(10,9) + 7;
+
   public int getResult() {
     //implement this
-    return 1;
+    return (int) product;
   }
 
   public void visitNode(TreeNode node) {
     //implement this
+    if(node.getColor() == Color.RED)
+      product = (product * node.getValue()) % modulo;
   }
 
   public void visitLeaf(TreeLeaf leaf) {
     //implement this
+    if(leaf.getColor() == Color.RED)
+      product = (product * leaf.getValue()) % modulo;
+
   }
 }
 
 class FancyVisitor extends TreeVis {
+  private int sumNodes = 0;
+  private int sumLeafs = 0;
+
   public int getResult() {
     //implement this
-    return 0;
+    return Math.abs(sumLeafs - sumNodes);
   }
 
   public void visitNode(TreeNode node) {
     //implement this
+    if(node.getDepth() % 2 == 0)
+      sumNodes += node.getValue();
   }
 
   public void visitLeaf(TreeLeaf leaf) {
     //implement this
+    if(leaf.getColor() == Color.GREEN)
+      sumLeafs += leaf.getValue();
   }
 }
 
 public class VisitorPatternExample {
+  private static int nodeValues[];
+  private static Color nodeColors[];
+  private static Map<Integer, Set<Integer>> nodesMap = new HashMap<>();
+
+  public static void createEdge(Tree parentNode, Integer nodeId){
+    Set<Integer> nodeEdges = nodesMap.get(nodeId);
+
+    if(nodeEdges != null && !nodeEdges.isEmpty()){
+      TreeNode node = new TreeNode(nodeValues[nodeId - 1], nodeColors[nodeId - 1], parentNode.getDepth() + 1);
+      ((TreeNode) parentNode).addChild(node);
+
+      for(Integer i: nodeEdges){
+        nodesMap.get(i).remove(nodeId);
+        createEdge(node, i);
+      }
+    }else{
+      TreeLeaf leaf = new TreeLeaf(nodeValues[nodeId - 1], nodeColors[nodeId - 1], parentNode.getDepth() + 1);
+      ((TreeNode) parentNode).addChild(leaf);
+    }
+  }
 
   public static Tree solve(){
     // Read the tree from STDIN and return its root as a return value of this function.
     Scanner in = new Scanner(System.in);
+
     int nodes = in.nextInt();
-    String[] values = in.nextLine().split(" ");
-    String[] colors = in.nextLine().split(" ");
-    HashMap<Integer,ArrayList<Integer>> relations = new HashMap<>();
+    nodeValues = new int[nodes];
+    nodeColors = new Color[nodes];
 
-    while(in.hasNext()){
-      int treeNodeIndex = in.nextInt() - 1;
-      int childIndex = in.nextInt() - 1;
+    for(int i = 0; i < nodes; i++)
+      nodeValues[i] = in.nextInt();
 
-      if(relations.containsKey(treeNodeInd))
+    for(int i = 0; i < nodes; i++)
+      nodeColors[i] = (in.nextInt() == 0) ? Color.RED : Color.GREEN;
 
+    Tree root;
 
-      TreeNode item = items.get(treeNodeIndex);
-      TreeNode child = items.get(childIndex);
+    if(nodes == 1){
+      root = new TreeLeaf(nodeValues[0], nodeColors[0], 0);
+    }else{
+      while(in.hasNextInt()){
+        int u = in.nextInt();
+        int v = in.nextInt();
+        Set<Integer> uEdges = nodesMap.get(u);
 
-      item.addChild(child);
+        if(uEdges == null)
+          uEdges = new HashSet<>();
+
+        uEdges.add(v);
+        nodesMap.put(u, uEdges);
+        Set<Integer> vEdges = nodesMap.get(v);
+
+        if(vEdges == null)
+          vEdges = new HashSet<>();
+
+        vEdges.add(u);
+        nodesMap.put(v, vEdges);
+      }
+
+      root = new TreeNode(nodeValues[0], nodeColors[0], 0);
+
+      if(nodesMap.containsKey(1)){
+        for(Integer edge: nodesMap.get(1)){
+          nodesMap.get(edge).remove(1);
+          createEdge(root, edge);
+        }
+      }
     }
 
-    // Return Root tree
-    return items.get(0);
+    return root;
   }
 
   public static void main(String[] args) {
